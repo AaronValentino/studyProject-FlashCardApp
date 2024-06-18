61a7086858a2bc2142221dfb63f0660eeb81a7b3
AaronValentino <aaronvalentino@i3-systems.com>
2024-06-18 16:55:58 +0900

    Add and sync gradle for Kotlin 2.0.0, Compose Compiler and Splash Screen API

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.compose.compiler)
 }
 
 android {
@@ -50,15 +51,22 @@ android {
 }
 
 dependencies {
-
+    // Core library
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
+
+    // Compose
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+
+    // Splash Screen
+    implementation(libs.splashscreen)
+
+    // Test
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
