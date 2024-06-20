7320a57a7d313850451d804b539ff2d8f5347198
AaronValentino <aaronvalentino@i3-systems.com>
2024-06-20 09:22:53 +0900

    Add color, typography and shape to theme

---
+++ app/src/main/java/com/example/flashcardapp/ui/theme/Shape.kt
@@ -0,0 +1,10 @@
+package com.example.flashcardapp.ui.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Shapes
+import androidx.compose.ui.unit.dp
+
+// Use to change predefined shapes only
+val Shape = Shapes(
+    small = RoundedCornerShape(50.dp)
+)
--- app/src/main/java/com/example/flashcardapp/ui/theme/Type.kt
+++ app/src/main/java/com/example/flashcardapp/ui/theme/Type.kt
@@ -1,34 +1,44 @@
 package com.example.flashcardapp.ui.theme
 
 import androidx.compose.material3.Typography
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
+import com.example.flashcardapp.R
+import com.example.flashcardapp.ui.HomeScreen
+
+val Poppins = FontFamily(
+    Font(resId = R.font.poppins_regular, weight = FontWeight.Normal),
+    Font(resId = R.font.poppins_semibold, weight = FontWeight.SemiBold),
+    Font(resId = R.font.poppins_black, weight = FontWeight.Black)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
+    titleLarge = TextStyle(
+        fontFamily = Poppins,
+        fontWeight = FontWeight.Black,
+        fontSize = 32.sp,
+        lineHeight = 28.sp,
+        letterSpacing = -1.sp
+    ),
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = Poppins,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 20.sp,
+        lineHeight = 20.sp,
+        letterSpacing = 1.sp
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = Poppins,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
         lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+        letterSpacing = 0.sp
     )
-    */
 )
