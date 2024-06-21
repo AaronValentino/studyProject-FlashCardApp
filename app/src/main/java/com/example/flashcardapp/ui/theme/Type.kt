fc5df0a262055fb2a152550a4976d01643c58b2f
AaronValentino <aaronvalentino@i3-systems.com>
2024-06-21 12:20:19 +0900

    Remove redundant imports in Type.kt

--- app/src/main/java/com/example/flashcardapp/ui/theme/Type.kt
+++ app/src/main/java/com/example/flashcardapp/ui/theme/Type.kt
@@ -1,16 +1,12 @@
 package com.example.flashcardapp.ui.theme
 
 import androidx.compose.material3.Typography
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import com.example.flashcardapp.R
-import com.example.flashcardapp.ui.HomeScreen
 
 val Poppins = FontFamily(
     Font(resId = R.font.poppins_regular, weight = FontWeight.Normal),
