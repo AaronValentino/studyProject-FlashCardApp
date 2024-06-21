9a30f014ea6ac278a6f7448757c146a61ca17d30
AaronValentino <aaronvalentino@i3-systems.com>
2024-06-21 12:56:14 +0900

    Create Deck.kt which contains the data class Deck and a createDeck() function, which is used to create 3 Decks that are in Deck.kt too

---
+++ app/src/main/java/com/example/flashcardapp/deck/Deck.kt
@@ -0,0 +1,31 @@
+package com.example.flashcardapp.deck
+
+import com.example.flashcardapp.card.Card
+import com.example.flashcardapp.card.createCardsData
+
+private var deckCount = 0
+
+data class Deck(
+    val deckID: Int,
+    val deckName: String,
+    val deckCards: List<Card>
+)
+
+object Deck1 {
+    val deck: Deck = createDeck()
+}
+
+object Deck2 {
+    val deck: Deck = createDeck()
+}
+
+object Deck3 {
+    val deck: Deck = createDeck()
+}
+
+fun createDeck(): Deck {
+    deckCount++
+    val deckID = deckCount
+
+    return Deck(deckID, "Deck $deckID", createCardsData(1))
+}
