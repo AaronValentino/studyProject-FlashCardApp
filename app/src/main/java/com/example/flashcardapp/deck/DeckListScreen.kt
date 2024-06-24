dc3c915eb2ca2702c48fbbb69600420128c8cb64
AaronValentino <aaronvalentino@i3-systems.com>
2024-06-24 22:13:07 +0900

    Update the screen and view model for listing all decks so that the viewmodel collects data from the database and the screen displays the data

--- app/src/main/java/com/example/flashcardapp/deck/DeckListScreen.kt
+++ app/src/main/java/com/example/flashcardapp/deck/DeckListScreen.kt
@@ -1,56 +1,78 @@
 package com.example.flashcardapp.deck
 
+import android.util.Log
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Card
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.flashcardapp.AppViewModelProvider
+import androidx.compose.material3.MaterialTheme as MaterialTheme1
 
 @Composable
-fun DeckListScreen() {
-    Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
-    ){
-        LazyVerticalGrid(
-            columns = GridCells.Adaptive(minSize = 150.dp),
-            contentPadding = PaddingValues(20.dp)
-        ) {
-            items(Deck1.deck.deckCards) {
-                var print by rememberSaveable {
-                    mutableStateOf(it.question)
-                }
-                val cardColor = if (print == it.question)
-                    MaterialTheme.colorScheme.secondaryContainer
-                else
-                    MaterialTheme.colorScheme.tertiaryContainer
+fun DeckListScreen(
+    topBar: @Composable () -> Unit,
+    backgroundBrush: Brush,
+    cardClicked: (Int) -> Unit,
+    addNewDeckClicked: (Int) -> Unit,
+    viewModel: DeckListViewModel = viewModel(factory = AppViewModelProvider.Factory)
+) {
+    val uiState = viewModel.deckListUiState.collectAsState()
+    val deckSize = (uiState.value.listItem.size + 1)
+    Log.d("Check Deck Size", deckSize.toString())
 
-                GenerateCard(
-                    print = print,
-                    cardClicked = {
-                        print = if (print == it.question) it.answer else it.question
-                    },
-                    modifier = Modifier
-                        .background(color = cardColor)
-                )
+    Scaffold(
+        topBar = topBar
+    ) { paddingValues ->
+        Box(
+            modifier = Modifier
+                .padding(paddingValues)
+                .background(brush = backgroundBrush)
+                .fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ){
+            LazyVerticalGrid(
+                columns = GridCells.Adaptive(minSize = 150.dp),
+                contentPadding = PaddingValues(12.dp)
+            ) {
+                items(uiState.value.listItem) {
+                    GenerateCard(
+                        deckName = it.name,
+                        numOfCards = it.numOfCards,
+                        cardClicked = { cardClicked(it.deckId) }
+                    )
+                }
+
+                item {
+                    GenerateCardAddNewDeck(
+                        addNewDeckClicked = { addNewDeckClicked(deckSize) }
+                    )
+                }
             }
         }
     }
@@ -58,23 +80,69 @@ fun DeckListScreen() {
 
 @Composable
 private fun GenerateCard(
-    print: String,
+    deckName: String,
+    numOfCards: Int,
     cardClicked: () -> Unit,
     modifier: Modifier = Modifier
 ) {
     Card(
-        modifier = Modifier
+        modifier = modifier
             .padding(16.dp)
-            .aspectRatio(1f)
+            .aspectRatio(0.75f),
+        border = BorderStroke(
+            width = 4.dp,
+            color = MaterialTheme1.colorScheme.primary
+        )
     ) {
         Column(
-            modifier = modifier
+            modifier = Modifier
                 .clickable(onClick = cardClicked)
                 .fillMaxSize()
-                .wrapContentSize()
+                .padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
             Text(
-                text = print,
+                text = deckName,
+                style = MaterialTheme1.typography.displaySmall,
+                overflow = TextOverflow.Ellipsis,
+                textAlign = TextAlign.Center
+            )
+            Spacer(modifier = Modifier.weight(1f))
+            Text(
+                text = "$numOfCards cards",
+                style = MaterialTheme1.typography.bodyMedium
+            )
+        }
+    }
+}
+
+@Composable
+private fun GenerateCardAddNewDeck(
+    addNewDeckClicked: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Card(
+        modifier = modifier
+            .padding(16.dp)
+            .aspectRatio(0.75f),
+        border = BorderStroke(
+            width = 4.dp,
+            color = MaterialTheme1.colorScheme.primary
+        )
+    ) {
+        Column(
+            modifier = Modifier
+                .clickable(onClick = addNewDeckClicked)
+                .fillMaxSize()
+                .padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Icon(
+                imageVector = Icons.Default.Add,
+                contentDescription = "null",
+                modifier = Modifier.size(50.dp),
+                tint = MaterialTheme1.colorScheme.primary
             )
         }
     }
--- app/src/main/java/com/example/flashcardapp/deck/DeckListViewModel.kt
+++ app/src/main/java/com/example/flashcardapp/deck/DeckListViewModel.kt
@@ -1,12 +1,28 @@
 package com.example.flashcardapp.deck
 
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
-import com.example.flashcardapp.card.Card
+import androidx.lifecycle.viewModelScope
+import com.example.flashcardapp.data.Deck
+import com.example.flashcardapp.data.DeckNCardRepository
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
 
-class DeckListViewModel() : ViewModel() {
+class DeckListViewModel(deckNCardRepository: DeckNCardRepository) : ViewModel() {
+    val deckListUiState: StateFlow<DeckListUiState> =
+        deckNCardRepository.getAllDecksStream().map {
+            DeckListUiState(it)
+        }.stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(TIMEOUT_MILLIS),
+            initialValue = DeckListUiState()
+        )
+    companion object {
+        private const val TIMEOUT_MILLIS = 5_000L
+    }
 }
 
-data class DeckListUiState(val listItem: List<Card> = listOf())
+data class DeckListUiState(
+    val listItem: List<Deck> = listOf()
+)
