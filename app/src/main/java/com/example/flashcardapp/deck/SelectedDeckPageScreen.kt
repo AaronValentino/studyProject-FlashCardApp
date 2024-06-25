9de913f16d4f6d056d6c78a6022a7799ae4a8fec
AaronValentino <aaronvalentino@i3-systems.com>
2024-06-25 15:23:47 +0900

    Add logic for the middle part of the screen which shows the deck's cards using a Lazy Row but only if when the list of cards is not empty. If it is empty, it will display an AddNewCard button. Also, add animation logic for when the card is toggle, the background color will change according to if it is displaying the question or the answer. At the top end of each card, a floating action button is placed for the user to navigate to the screen for editing that card.

--- app/src/main/java/com/example/flashcardapp/deck/SelectedDeckPageScreen.kt
+++ app/src/main/java/com/example/flashcardapp/deck/SelectedDeckPageScreen.kt
@@ -6,8 +6,16 @@ import androidx.compose.animation.core.InfiniteRepeatableSpec
 import androidx.compose.animation.core.RepeatMode
 import androidx.compose.animation.core.rememberInfiniteTransition
 import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.scaleIn
+import androidx.compose.animation.scaleOut
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
+import androidx.compose.animation.togetherWith
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -20,6 +28,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Close
@@ -34,20 +43,28 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.State
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.flashcardapp.AppViewModelProvider
+import com.example.flashcardapp.ui.theme.Shape
 
 @Composable
 fun SelectedDeckPageScreen(
     backgroundBrush: Brush,
     onClickedBack: () -> Unit,
     onClickedEditDeck: () -> Unit,
+    onClickedAddNewCard: (Int, String) -> Unit,
     onClickedAllCards: () -> Unit,
     onClickedLesson: () -> Unit,
     viewModel: SelectedDeckAndCardsDetailsViewModel = viewModel(factory = AppViewModelProvider.Factory)
@@ -55,7 +72,7 @@ fun SelectedDeckPageScreen(
     val uiState = viewModel.selectedDeckCardsUiState.collectAsState()
 
     val infiniteTransition = rememberInfiniteTransition(label = "")
-    val infiniteRotationBrush = infiniteTransition.animateColor(
+    val infiniteRotationColor = infiniteTransition.animateColor(
         initialValue = MaterialTheme.colorScheme.primary,
         targetValue = MaterialTheme.colorScheme.onPrimary,
         animationSpec = InfiniteRepeatableSpec(
@@ -93,7 +110,7 @@ fun SelectedDeckPageScreen(
                         modifier = Modifier.fillMaxWidth()
                     )
                     Text(
-                        text = "${uiState.value.selectedDeck.numOfCards} Cards",
+                        text = "${uiState.value.deckCards.size} Card(s)",
                         style = MaterialTheme.typography.bodyLarge,
                         maxLines = 2,
                         textAlign = TextAlign.Center,
@@ -132,50 +149,29 @@ fun SelectedDeckPageScreen(
                         )
                     }
                     Spacer(modifier = Modifier.weight(1f))
-
-                    LazyRow(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .aspectRatio(0.85f),
-                        contentPadding = PaddingValues(
-                            vertical = 16.dp),
-                        horizontalArrangement = Arrangement.Start,
-                        verticalAlignment = Alignment.CenterVertically
-                    ) {
-                        item(0) {
-                            AnimatedContent(targetState = infiniteRotationBrush, label = "") {
-                                Card(
-                                    modifier = Modifier
-                                        .fillMaxHeight(0.8f)
-                                        .aspectRatio(0.2f),
-                                    border = BorderStroke(
-                                        width = 2.dp,
-                                        color = it.value
-                                    )
-                                ) {
-                                    Icon(
-                                        imageVector = Icons.Default.Add,
-                                        contentDescription = "Add cards",
-                                        modifier = Modifier
-                                            .fillMaxSize()
-                                            .padding(8.dp)
-                                    )
-                                }
+                    if (uiState.value.deckCards.isEmpty()) {
+                        AddNewCard(
+                            infiniteRotationColor = infiniteRotationColor,
+                            onClickedAddNewCard = {
+                                onClickedAddNewCard(
+                                    uiState.value.selectedDeck.deckId,
+                                    uiState.value.selectedDeck.name
+                                )
                             }
-                        }
-                        (1..20).map {
-                            items(it) {
-                                Card(
-                                    modifier = Modifier
-                                        .fillMaxHeight(0.8f)
-                                        .aspectRatio(0.8f)
-                                        .padding(horizontal = 16.dp)
-                                ) {
-                                    Text("it")
-                                }
+                        )
+                    } else {
+                        GenerateLazyRowForCards(
+                            infiniteRotationColor = infiniteRotationColor,
+                            uiState = uiState,
+                            onClickedAddNewCard = {
+                                onClickedAddNewCard(
+                                    uiState.value.selectedDeck.deckId,
+                                    uiState.value.selectedDeck.name
+                                )
                             }
-                        }
+                        )
                     }
+
                     Spacer(modifier = Modifier.weight(1f))
                     Row(
                         modifier = Modifier.fillMaxWidth(),
@@ -230,6 +226,152 @@ fun SelectedDeckPageScreen(
                     contentDescription = "Close Button",
                     tint = MaterialTheme.colorScheme.primary
                 )
+            }
+        }
+    }
+}
+
+@Composable
+fun AddNewCard(
+    infiniteRotationColor: State<Color>,
+    onClickedAddNewCard: () -> Unit
+) {
+    AnimatedContent(targetState = infiniteRotationColor, label = "") {
+        Card(
+            modifier = Modifier
+                .clickable { onClickedAddNewCard() }
+                .fillMaxHeight(0.8f)
+                .fillMaxWidth(0.7f)
+                .padding(vertical = 16.dp),
+            border = BorderStroke(
+                width = 4.dp,
+                color = it.value
+            )
+        ) {
+            Icon(
+                imageVector = Icons.Default.Add,
+                contentDescription = "Add cards",
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(40.dp)
+            )
+        }
+    }
+}
+
+@Composable
+fun GenerateLazyRowForCards(
+    infiniteRotationColor: State<Color>,
+    uiState: State<SelectedDeckUiState>,
+    onClickedAddNewCard: () -> Unit
+) {
+    LazyRow(
+        modifier = Modifier
+            .fillMaxWidth()
+            .aspectRatio(0.85f),
+        contentPadding = PaddingValues(
+            vertical = 16.dp),
+        horizontalArrangement = Arrangement.Start,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        item("add_new_card") {
+            AnimatedContent(targetState = infiniteRotationColor, label = "") {
+                Card(
+                    modifier = Modifier
+                        .clickable { onClickedAddNewCard() }
+                        .fillMaxHeight(0.8f)
+                        .aspectRatio(0.2f),
+                    border = BorderStroke(
+                        width = 2.dp,
+                        color = it.value
+                    )
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Add,
+                        contentDescription = "Add cards",
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(8.dp)
+                    )
+                }
+            }
+        }
+        items(uiState.value.deckCards) {
+            var contentDisplay by rememberSaveable {
+                mutableStateOf(it.question)
+            }
+            Box(
+                modifier = Modifier
+                    .fillMaxHeight(),
+                contentAlignment = Alignment.Center
+            ) {
+                AnimatedContent(
+                    targetState = contentDisplay,
+                    label = "",
+                    transitionSpec = {
+                        (
+                            slideInVertically()
+                            +
+                            fadeIn(animationSpec = tween(500))
+                            +
+                            scaleIn(initialScale = 0.5f, animationSpec = tween(500))
+                        )
+                        .togetherWith(
+                            slideOutVertically { it }
+                            +
+                            fadeOut(animationSpec = tween(500))
+                            +
+                            scaleOut(targetScale = 0.5f, animationSpec = tween(500))
+                        )
+                    },
+                ) { string ->
+                    Card(
+                        modifier = Modifier
+                            .clickable {
+                                contentDisplay = if (contentDisplay == it.question) {
+                                    it.answer
+                                } else {
+                                    it.question
+                                }
+                            }
+                            .fillMaxSize(0.8f)
+                            .aspectRatio(0.85f)
+                            .padding(horizontal = 20.dp),
+                        colors = CardDefaults.cardColors(
+                            containerColor = if (contentDisplay == it.question) {
+                                MaterialTheme.colorScheme.secondaryContainer
+                            } else {
+                                MaterialTheme.colorScheme.primary
+                            }
+                        )
+                    ) {
+                        Column(
+                            modifier = Modifier
+                                .fillMaxSize(),
+                            verticalArrangement = Arrangement.Center
+                        ) {
+                            Text(
+                                text = string,
+                                modifier = Modifier
+                                    .fillMaxWidth(),
+                                textAlign = TextAlign.Center
+                            )
+                        }
+                    }
+                }
+                FloatingActionButton(
+                    onClick = { },
+                    modifier = Modifier
+                        .align(Alignment.TopEnd)
+                        .padding(start = 4.dp),
+                    shape = Shape.small
+                ) {
+                    Icon(
+                        imageVector = Icons.Filled.Edit,
+                        contentDescription = "Close Button",
+                        tint = MaterialTheme.colorScheme.primary
+                    )
+                }
             }
         }
     }
